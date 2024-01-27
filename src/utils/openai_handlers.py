dc0c7150b8f3b34a2b3fef4d3ccb273a8140efe2
Максим Халёса <playwright@inbox.ru>
2024-01-27 23:52:39 +0300

    Fix openai handlers bugs.

--- src/utils/openai_handlers.py
+++ src/utils/openai_handlers.py
@@ -7,16 +7,16 @@ logger = logging.getLogger(__name__)
 
 async def send_msg_to_gpt(new: str, prev: str | None, username: str) -> str:
     '''
-    Accepts new and previous user mesages and send them to GPT.
+    Accepts new and previous user mesages and sends them to GPT.
 
-    Return text of gpt response.
+    Returns text of gpt response.
     '''
     try:
         logger.info('Sending msg to gpt...')
         response = await gpt_client.chat.completions.create(
             messages=[
-                {'role': 'user', 'content': prev},  # 'name': username},
-                {'role': 'user', 'content': new}  # 'name': username}
+                {'role': 'user', 'content': prev, 'name': username},
+                {'role': 'user', 'content': new, 'name': username}
             ],
             model='gpt-4',
             temperature=5
